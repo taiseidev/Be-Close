2a933772f1ded250ebe19080278a720434fd6f38
onishi taisei <taisei.developer@gmail.com>
2022-07-08 21:45:37 +0900

    🚀 feat: create User domain object

---
+++ lib/data/model/user_model.dart
@@ -0,0 +1,30 @@
+// ignore: unused_import
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:my_sns_project/data/model/json_converters.dart';
+
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
+
+@freezed
+class UserModel with _$UserModel {
+  const factory UserModel({
+    required String id,
+    required String name,
+    required String phoneNum,
+    required String dateOfBirth,
+    required int age,
+    required int countryNum,
+    String? iconUrl,
+    @Default(0) int status,
+    @Default(0) int postNum,
+    @Default(false) bool isCompleted,
+    @Default(true) bool isAvailable,
+    @TimestampDateTimeConverter() DateTime? createdAt,
+    @TimestampDateTimeConverter() DateTime? updatedAt,
+  }) = _UserModel;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+}
