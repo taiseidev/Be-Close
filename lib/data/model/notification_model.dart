31384652e798939f696394af8c7f25092c571ad8
onishi taisei <taisei.developer@gmail.com>
2022-07-08 21:58:53 +0900

    🚀 feat: create Notification domain object

---
+++ lib/data/model/notification_model.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:my_sns_project/data/model/json_converters.dart';
+
+part 'notification_model.freezed.dart';
+part 'notification_model.g.dart';
+
+@freezed
+class NotificationModel with _$NotificationModel {
+  const factory NotificationModel({
+    required String id,
+    required String title,
+    required String fromUser,
+    required bool type,
+    @Default(false) bool isRead,
+    String? content,
+    @TimestampDateTimeConverter() DateTime? createdAt,
+    @TimestampDateTimeConverter() DateTime? updatedAt,
+  }) = _NotificationModel;
+
+  factory NotificationModel.fromJson(Map<String, dynamic> json) =>
+      _$NotificationModelFromJson(json);
+}
