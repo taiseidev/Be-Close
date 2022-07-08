3c9ff8810017530d3e33c9b9609cad8cdecf8177
onishi taisei <taisei.developer@gmail.com>
2022-07-08 22:35:18 +0900

    🚀 feat: create Follow domain object

---
+++ lib/data/model/follow_model.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:my_sns_project/data/model/json_converters.dart';
+
+part 'follow_model.freezed.dart';
+part 'follow_model.g.dart';
+
+@freezed
+class FollowModel with _$FollowModel {
+  const factory FollowModel({
+    required String id,
+    required String following,
+    required String followed,
+    @TimestampDateTimeConverter() DateTime? createdAt,
+    @TimestampDateTimeConverter() DateTime? updatedAt,
+  }) = _FollowModel;
+
+  factory FollowModel.fromJson(Map<String, dynamic> json) =>
+      _$FollowModelFromJson(json);
+}
