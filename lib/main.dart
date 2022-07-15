5deddb82459185a119e7a5125037e196ed6fbb7c
onishi taisei <taisei.developer@gmail.com>
2022-07-16 01:18:27 +0900

    🚜 refactor: remove unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:my_sns_project/app.dart';
 import 'package:my_sns_project/gen/firebase_options_prod.dart' as prod;
 import 'package:my_sns_project/gen/firebase_options_stg.dart' as stg;
-import 'package:my_sns_project/router/error_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: getFirebaseOptions());
-  runApp(MyApp());
+  runApp(App());
 }
 
+// 環境によってFirebaseプロジェクトを切り替え
 FirebaseOptions getFirebaseOptions() {
   const flavor = String.fromEnvironment('FLAVOR');
   switch (flavor) {
@@ -21,91 +20,5 @@ FirebaseOptions getFirebaseOptions() {
       return prod.DefaultFirebaseOptions.currentPlatform;
     default:
       throw ArgumentError('Not available flavor');
-  }
-}
-
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  @override
-  Widget build(BuildContext context) => MaterialApp.router(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        routeInformationProvider: _router.routeInformationProvider,
-        routeInformationParser: _router.routeInformationParser,
-        routerDelegate: _router.routerDelegate,
-      );
-  final _router = GoRouter(
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const MyHomePage(
-          title: '',
-        ),
-      ),
-    ],
-    errorBuilder: (context, state) => const ErrorPage(),
-  );
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  String nam = '';
-
-  @override
-  void initState() {
-    super.initState();
-    getName();
-  }
-
-  Future<void> getName() async {
-    final data = await FirebaseFirestore.instance.collection('user').get();
-    final name = data.docs.first.data()['name'] as String;
-    setState(() {
-      nam = name;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              nam,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
   }
 }
