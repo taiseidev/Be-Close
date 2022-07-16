be31f830b3bb242a7ccfd50ad37f6de47fe16f94
onishi taisei <taisei.developer@gmail.com>
2022-07-16 14:08:38 +0900

    🚀 feat: create a file for each tab

---
+++ lib/presentation/pages/home/components/home_page_tab_third.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePageTabThird extends StatelessWidget {
+  const HomePageTabThird({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Hello World",
+      ),
+    );
+  }
+}
--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_sns_project/presentation/pages/home/components/home_page_app_bar.dart';
+import 'package:my_sns_project/presentation/pages/home/components/home_page_body.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,11 +10,13 @@ class HomePage extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
+        extendBodyBehindAppBar: true,
         backgroundColor: Colors.black,
-        appBar: PreferredSize(
+        appBar: const PreferredSize(
           preferredSize: Size(30, 100),
           child: HomePageAppBar(),
         ),
+        body: HomePageBody(),
       ),
     );
   }
